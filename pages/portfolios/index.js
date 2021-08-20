acbcbe0a5b700dd7500391a31530a0be412ed9dd
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-20 17:08:31 +0530

    Use SWR in custom hook

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -2,16 +2,15 @@
 import BaseLayout from '../../components/layouts/BaseLayouts'
 import BasePage from '../../components/BasePage'
 import Link from 'next/link'
-import { useState, useEffect } from 'react'
-import { useGetData } from '../../actions'
-import useSWR from 'swr'
+import { useGetPosts } from '../../actions'
 
 const fetcher = url => fetch(url).then(res=>res.json());
 
 export default function Portfolios() {
 
     // const { data, error, loading } = useGetData('/api/v1/posts');
-    const { data, error, loading } = useSWR('/api/v1/posts', fetcher);
+    const { data, error, loading } = useGetPosts();
+    // const { data, error, loading } = useSWR('/api/v1/posts', fetcher);
 
     const renderPosts = () => data.map(post => <li key={post.id} style={{ 'fontSize': '20px' }}>
         <Link href={`/portfolios/${post.id}`}>
