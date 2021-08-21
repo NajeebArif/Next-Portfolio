3d2052e7e83c997682e376ac019a23e69904b9a1
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-21 21:38:26 +0530

    Add custom server props
    Add SSR protected page and provide a custom server side props to as well along with the logged in user

--- pages/blogs.js
+++ pages/blogs.js
@@ -3,13 +3,49 @@
 import React from 'react'
 import BaseLayout from '@/components/layouts/BaseLayouts'
 import BasePage from '@/components/BasePage'
+import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 
-export default function Blogs() {
+export default function Blogs({ user, serverMsg }) {
     return (
         <BaseLayout>
             <BasePage>
-                <h1>I am blogs page</h1>
+                <BlogsContent user={user} msg={serverMsg}/>
             </BasePage>
         </BaseLayout >
     )
+}
+
+const BlogsContent = ({ user, msg }) => {
+    return (
+        <>
+            <h1>I am blogs page {user.email}</h1>
+            {
+                msg && <>
+                    <h3>You have one message:</h3>
+                    <p>{msg}</p>
+                </>
+            }
+
+        </>
+    )
+}
+
+export const getServerSideProps = withPageAuthRequired({
+    getServerSideProps: async (ctx) => {
+        const msg = await getLatestMsg();
+        return {
+            props: {
+                serverMsg: msg
+            }
+        }
+    }
+});
+
+const getLatestMsg = () => {
+    return new Promise((res) => {
+        const time = new Date().toISOString();
+        setTimeout(() => {
+            res(`My Latest server message at: ${time}!`)
+        }, 500)
+    })
 }
