f41ef9078034df5f055c6669eba6f7e20309956e
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-21 20:57:15 +0530

    IMP: Call Protected API route From UI
    Call Protected API route from CSR Page which is also protected by withPageAuthRequired

--- pages/about.js
+++ pages/about.js
@@ -3,13 +3,38 @@
 import React from 'react'
 import BaseLayout from '@/components/layouts/BaseLayouts'
 import BasePage from '@/components/BasePage'
+import useSWR from 'swr'
+import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 
-export default function About() {
+export default withPageAuthRequired(function About() {
     return (
         <BaseLayout>
             <BasePage>
-                <h1>I am about page</h1>
+                <AboutData />
             </BasePage>
         </BaseLayout >
+    )
+});
+
+const fetcher = async (uri) =>{
+    const res = await fetch(uri);
+    return res.json();
+}
+
+const AboutData = () =>{
+    const {data, error} = useSWR('api/v1/protected',fetcher);
+    return (
+        <>
+            {
+                error && <div>oops... {error.message}</div>
+            }
+            {
+                !data ? <p>Loading...</p> :
+                <>
+                    <h1>I am about page</h1>
+                    <p>Welcome {data.email} . This is your protect message{data.protected}</p>
+                </> 
+            }
+        </>
     )
 }
