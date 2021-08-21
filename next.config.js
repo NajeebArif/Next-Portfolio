1f8836b451e14d3b48ad3045c93026fa6948afaa
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-21 14:06:28 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 
 
 const path = require('path')
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   webpack: config => {
     config.resolve.alias['@'] = path.resolve(__dirname);
+    config.plugins.push(new Dotenv({silent:true}));
     return config;
   }
 }
