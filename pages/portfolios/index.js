4f2a5f2dee89e5a808a8ca3ae9ff6f07119d4da4
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-26 00:51:15 +0530

    Create Portfolio Form

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -5,13 +5,15 @@ import { useRouter } from 'next/router';
 import PortfolioApi from '../../libs/api/portfoliosApi'
 import { Row, Col } from 'reactstrap';
 import PortfolioCard from '@/components/PortfolioCard';
+import { useUser } from '@auth0/nextjs-auth0';
+import Link from 'next/link'
 
 export default function Portfolios({ portfolios }) {
 
     const router = useRouter()
     return (
         <BaseLayout>
-            <BasePage header="Portfolios" className="portfolio-page">
+            <BasePage header={<PortoliosHeader />} className="portfolio-page">
                 <Row>
                     {portfolios.map(portfolio =>
                         <Col
@@ -28,6 +30,29 @@ export default function Portfolios({ portfolios }) {
             </BasePage>
         </BaseLayout >
     )
+}
+
+const PortoliosHeader = () => {
+    const { user, error, isLoading } = useUser();
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>{error.message}</div>;
+
+    if(user){
+        return (
+            <>
+                <span className="mr-5">Protfolios</span>
+                <span className="ml-5">
+                    <Link href="/portfolios/new">
+                    <button className="btn btn-primary">Create</button>
+                    </Link>
+                </span>
+            </>
+        )
+    }else{
+        return "Portfolio"
+    }
+
+
 }
 
 export async function getStaticProps() {
---
+++ pages/portfolios/new.js
@@ -0,0 +1,23 @@
+import BaseLayout from '@/components/layouts/BaseLayouts';
+import BasePage from '@/components/BasePage';
+import { Row, Col } from 'reactstrap';
+import PortfolioForm from '@/components/PortfolioForm';
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+
+export default withPageAuthRequired(function PortfolioNew({user}) {
+
+    const roleKey = process.env.AUTH0_NAMESPACE;
+    const isAdmin = user[roleKey]?.includes('admin')
+
+    return (
+        <BaseLayout >
+            <BasePage header={isAdmin?"Create Portfolio":"You can not create portfolios!!"}>
+                <Row>
+                    <Col md="8">
+                        {isAdmin && <PortfolioForm />}
+                    </Col>
+                </Row>
+            </BasePage>
+        </BaseLayout>
+    )
+})
