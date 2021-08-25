baa949d7618cf21cdd5c233f6ec1c7b6cc5fce32
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-25 23:37:17 +0530

    Use getStaticPath and getStaticProps

--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -4,7 +4,7 @@ import BasePage from '@/components/BasePage'
 import PortfolioApi from '@/libs/api/portfoliosApi'
 
 
-const Portfolio = ({portfolio}) => {
+const Portfolio = ({ portfolio }) => {
     return (
         <BaseLayout>
             <BasePage header="Portfolio Detail">
@@ -16,11 +16,33 @@ const Portfolio = ({portfolio}) => {
     )
 }
 
-export async function getServerSideProps({query}) {
-    const json = await new PortfolioApi().getById(query.id);
+export async function getStaticPaths() {
+    const json = await new PortfolioApi().getAll();
+    const portfolios = json.data;
+
+    const paths = portfolios.map(p => {
+        return {
+            params: { id: p._id }
+        }
+    })
+    return { paths, fallback: false }
+}
+
+export async function getStaticProps({params}){
+    const json = await new PortfolioApi().getById(params.id)
     const portfolio = json.data;
-  
-    return {props: { portfolio }};
-  }
+    return {
+        props: {
+            portfolio
+        }
+    }
+}
+
+// export async function getServerSideProps({query}) {
+//     const json = await new PortfolioApi().getById(query.id);
+//     const portfolio = json.data;
+
+//     return {props: { portfolio }};
+//   }
 
 export default Portfolio;
