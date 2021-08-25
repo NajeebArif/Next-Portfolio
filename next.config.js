2c0de75777e78ada230c01f0c18d092e27cf4848
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-26 00:51:04 +0530

    Update next.config.js
    Expose env variables to next frontent

--- next.config.js
+++ next.config.js
@@ -7,5 +7,8 @@ module.exports = {
   webpack: config => {
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
+  },
+  env: {
+    AUTH0_NAMESPACE: process.env.AUTH0_NAMESPACE
   }
 }
