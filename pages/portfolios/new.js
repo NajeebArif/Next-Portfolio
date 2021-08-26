9b360bb2568117ef105249ea811e0e0835be3895
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-26 14:30:20 +0530

    Add proxy api to make request to backend

--- pages/portfolios/new.js
+++ pages/portfolios/new.js
@@ -3,22 +3,23 @@ import BasePage from '@/components/BasePage';
 import { Row, Col } from 'reactstrap';
 import PortfolioForm from '@/components/PortfolioForm';
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { createPortfolio } from '@/actions/portfolios';
 
 export default withPageAuthRequired(function PortfolioNew({ user }) {
 
     const roleKey = process.env.AUTH0_NAMESPACE;
     const isAdmin = user[roleKey]?.includes('admin')
 
-    const createPortfolio = (data) => {
-        alert(JSON.stringify(data));
-    }
+    const _createPortfolio = (data) => {
+        createPortfolio(data)
+      }
 
     return (
         <BaseLayout >
             <BasePage header={isAdmin ? "Create Portfolio" : "You can not create portfolios!!"}>
                 <Row>
                     <Col md="8">
-                        {isAdmin && <PortfolioForm onSubmit={createPortfolio}/>}
+                        {isAdmin && <PortfolioForm onSubmit={_createPortfolio}/>}
                     </Col>
                 </Row>
             </BasePage>
